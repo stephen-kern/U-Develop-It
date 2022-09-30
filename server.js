c8cf22baf0f977bd4116a3017f121911fa9cf451
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-09-30 13:36:50 -0500

    connected the app to the sql database and tested it

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-// include packages 
-const express = require('express');
+// include packages
+const express = require("express");
+const mysql = require("mysql2");
 
 // PORT designation and initialize
 const PORT = process.env.PORT || 3001;
@@ -9,22 +10,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect app to SQL database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
-
-
-
-
-
-
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 // function to start the express server
 app.listen(PORT, () => {
-   console.log(`Server is running on ${PORT}`); 
+  console.log(`Server is running on ${PORT}`);
 });
